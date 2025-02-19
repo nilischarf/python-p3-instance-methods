cea3ff59d1fc6ce0c4513482285a68649613dda6
Nili Scharf <nilischarf@gmail.com>
2025-02-19 11:59:14 +0200

    complete instance methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    # Class body goes here 
+    def bark(self):
+        return print("Woof!")
+
+    def sit(self):
+        return print("The dog is sitting.")
 
     #Instance method definition
-    pass
+    
+fido = Dog()
+fido.bark()
+snoopy = Dog()
+snoopy.bark()
+    
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,11 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        return print("Hello World!")
+
+    def walk(self):
+        return print("The person is walking.")
 
     #Instance method definition
     pass
